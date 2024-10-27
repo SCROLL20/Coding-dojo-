205377795c263f62a0327e79b461fe13ad6d7116
SCROLL20 <80055303+SCROLL20@users.noreply.github.com>
2024-10-27 17:36:14 +0100

    Add files via upload

---
+++ javascript/super ninja.js
@@ -0,0 +1,35 @@
+class Ninja {
+    constructor(name, health) {
+        this.name = name;
+        this.health = health;
+        this.speed = 3;
+        this.strength = 3;
+    }
+    sayName() {
+        console.log(this.name);
+    }
+    showStats() {
+        console.log("Name: " + this.name + ", Health: " + this.health + ", Speed: " + this.speed + ", Strength: " + this.strength);
+    }
+    drinkSake() {
+        this.health += 10;
+    } 
+}
+
+class Sensei extends Ninja {
+    constructor(name) {
+        super(name,200);
+        this.speed = 10;
+        this.strength = 10;
+        this.wisdom = 10;
+    }
+    speakWisdom() {
+        this.drinkSake();
+        console.log("What one programmer can do in one month, two programmers can do in two months.")
+    }
+}
+
+
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+superSensei.showStats();
