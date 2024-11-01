24fdb10cb7f499e6e711ec6129c9c4a02e0b747a
unknown <louaidghim2@gmail.com>
2024-11-01 02:54:02 +0100

    test

---
+++ full stack mern/product manager 1/product-manager/server/models/product.js
@@ -0,0 +1,10 @@
+// server/models/Product.js
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    price: { type: Number, required: true },
+    description: { type: String, required: true }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Product', productSchema);
